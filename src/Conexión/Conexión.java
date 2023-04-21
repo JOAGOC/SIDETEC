21106e4ca850ab30ce8cf47919737b177f793c04
JOAGOC <joangonzalezcr@ittepic.edu.mx>
2023-04-20 20:32:08 -0700

    Se agrega la clase Usuario, la cual contiene las transacciones SQL para almacenar, modificar, eliminar y una consulta general vista desde la página principal. También se movió la clase conexión al paquete Conexión para poder importar estáticamente el método getConnection, que se implementa mediante Singleton (Una única instancia es inicializada y devuelta). Finalmente, en la clase Gestionar_Contraseña se mantienen los métodos de validación de contraseña y el método de encriptación por si se implementan en avances futuros

---
+++ src/Conexión/Conexión.java
@@ -0,0 +1,24 @@
+package Conexión;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Conexión {
+    private static Connection con;
+    
+    public static Connection getConnection() {
+        if (con!=null)
+            return con;
+        try {
+            String db = "jdbc:mysql://localhost:3306/consultorio_dental";
+            con = DriverManager.getConnection(db, "root", "root");
+            System.out.println("CONECTADO");
+            return con;
+        } catch (SQLException e) {
+            showMessageDialog(null, "Error en la conexion. " + e.getMessage());
+        }
+        return null;
+    }
+}
